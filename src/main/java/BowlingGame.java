8c72635e07a96f13b31338d8afd3f0ade821f310
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-22 17:27:19 +0000

    All calculate() lines at same level of abstraction

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,9 +1,16 @@
 import java.util.regex.Pattern;
 
 public class BowlingGame {
+
+    public static final char STRIKE = 'X';
+
     public int calculate(String scorecard) {
-        int total = 0;
         var frames = scorecard.split(Pattern.quote("|"));
+        return getTotal(frames);
+    }
+
+    private int getTotal(String[] frames) {
+        int total = 0;
         for (String frame: frames) {
             total += totalFor(frame);
         }
@@ -13,7 +20,7 @@ public class BowlingGame {
     private int totalFor(String frame) {
         int total = 0;
         for (char c: frame.toCharArray()) {
-            if (c == 'X') {
+            if (c == STRIKE) {
                 total += 10;
             } else if (Character.isDigit(c)) {
                 total += Integer.parseInt(String.valueOf(c));
