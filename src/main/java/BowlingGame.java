2cf15be61b1cd3c3d9117e5c20e35c656fe2f853
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-22 17:17:23 +0000

    Iterate by frame

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,12 +1,16 @@
+import java.util.regex.Pattern;
+
 public class BowlingGame {
     public int calculate(String scorecard) {
         int total = 0;
-        for (char c: scorecard.toCharArray()) {
-            if (c == 'X') {
-                total += 10;
-            }
-            else if (Character.isDigit(c)) {
-                total += Integer.parseInt(String.valueOf(c));
+        var frames = scorecard.split(Pattern.quote("|"));
+        for (String frame: frames) {
+            for (char c: frame.toCharArray()) {
+                if (c == 'X') {
+                    total += 10;
+                } else if (Character.isDigit(c)) {
+                    total += Integer.parseInt(String.valueOf(c));
+                }
             }
         }
         return total;
