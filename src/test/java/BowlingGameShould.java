b27291985563015d2a1d7794bce19c7fec982d05
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-22 17:50:04 +0000

    Double both balls in frame after strike

--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -17,6 +17,7 @@ public class BowlingGameShould {
             "X|--|--|--|--|--|--|--|--|--||, 10",
             "X|--|--|5-|5-|5-|5-|--|--|--||, 30",
             "X|5-|--|--|--|--|--|--|--|--||, 20",
+            "X|51|--|--|--|--|--|--|--|--||, 22",
     })
     public void add_numbers_correctly(String scorecard, int expectedTotal) {
         BowlingGame game = new BowlingGame();
