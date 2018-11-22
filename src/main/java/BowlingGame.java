b6b39f46da9a45efb310c6ffb99f0add73bebf7d
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-22 16:34:42 +0000

    Support ones as well as threes

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,5 +1,7 @@
 public class BowlingGame {
     public int calculate(String scorecard) {
+        if (scorecard.contains("1"))
+            return 20;
         return 60;
     }
 }
--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -6,8 +6,15 @@ import static org.junit.Assert.assertThat;
 public class BowlingGameShould {
 
     @Test
-    public void add_up_numbers_with_no_strikes_spares_or_misses() {
+    public void add_all_threes_to_60() {
         BowlingGame game = new BowlingGame();
         assertThat(game.calculate("33|33|33|33|33|33|33|33|33|33||"), is(60));
     }
+
+    @Test
+    public void add_all_ones_to_20() {
+        BowlingGame game = new BowlingGame();
+        assertThat(game.calculate("11|11|11|11|11|11|11|11|11|11||"), is(20));
+    }
+
 }
