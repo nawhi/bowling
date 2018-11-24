64d6416db4854dea668e955f688ee59987834327
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-24 12:18:38 +0000

    Use stream for score()

--- src/main/java/FrameList.java
+++ src/main/java/FrameList.java
@@ -1,23 +1,31 @@
+import java.util.stream.IntStream;
+
 public class FrameList {
     public static final char STRIKE = 'X';
     private static final char MISS = '-';
-    private final String balls;
 
-    private String scorecard;
+    private final String scorecard;
+    private final String balls;
+    private final int numRegularBalls;
 
     public FrameList(String scorecard) {
         this.scorecard = scorecard;
         this.balls = scorecard.replace("|", "");
+        this.numRegularBalls = getNumRegularBalls();
     }
 
     public int score() {
+        return IntStream.range(0, numRegularBalls)
+                .map(this::scoreBallAt)
+                .sum();
+    }
+
+    private int scoreBallAt(int ballIndex) {
         int total = 0;
-        for (int i = 0; i < getNumRegularBalls(); ++i) {
-            char ball = balls.charAt(i);
-            total += ballScore(ball);
-            if (ball == STRIKE) {
-                total += scoreStrikeAt(i);
-            }
+        char ball = balls.charAt(ballIndex);
+        total += ballScore(ball);
+        if (ball == STRIKE) {
+            total += scoreStrikeAt(ballIndex);
         }
         return total;
     }
